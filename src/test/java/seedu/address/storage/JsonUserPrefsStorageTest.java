b294745438d45dd1767fcdc7bee610aee051d057
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-10-23 15:20:51 +0800

    Fix failing tests due to filename capitalization

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setTruthTableFilePath(Paths.get("truthTable.json"));
+        userPrefs.setTruthTableFilePath(Paths.get("truthtable.json"));
         return userPrefs;
     }
 
